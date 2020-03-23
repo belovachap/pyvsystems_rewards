6fad671965e6c9607d55109ee6a65f5c69d696fe
Chapman Shoop <chapman.shoop@gmail.com>
2020-03-23 16:30:10 -0700

    Fix a bug in format_as_vsys for negative inputs.

--- pyvsystems_rewards/format.py
+++ pyvsystems_rewards/format.py
@@ -1,6 +1,10 @@
 
 def format_as_vsys(amount):
-    amount = int(amount)
-    whole = int(amount / 100000000)
-    fraction = amount % 100000000
+    abs_amount = abs(amount)
+    whole = int(abs_amount / 100000000)
+    fraction = abs_amount % 100000000
+
+    if amount < 0:
+        whole *= -1
+
     return f'{whole}.{str(fraction).rjust(8, "0")}'
--- test/test_format.py
+++ test/test_format.py
@@ -2,6 +2,8 @@ from pyvsystems_rewards.format import format_as_vsys
 
 
 def test_format_as_vsys():
-	assert format_as_vsys(100000000) == '1.00000000'
-	assert format_as_vsys(703) == '0.00000703'
-	assert format_as_vsys(4747) == '0.00004747'
+    assert format_as_vsys(100000000) == '1.00000000'
+    assert format_as_vsys(703) == '0.00000703'
+    assert format_as_vsys(4747) == '0.00004747'
+    assert format_as_vsys(123456789) == '1.23456789'
+    assert format_as_vsys(-123456789) == '-1.23456789'
